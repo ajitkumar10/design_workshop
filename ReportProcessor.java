a71eef226ea6b924b68c701279925e9946218de0
ajitkumar10 <ajitkumar10@gmail.com>
2017-04-01 23:37:09 -0400

    design updates

--- ReportProcessor.java
+++ ReportProcessor.java
@@ -9,10 +9,11 @@ Monitoring and Alerts
 ReportConfig
 	.id()
 	.rptName("report {report_type} - {report_sub_type}")
+	.rptType(OPEN_ORDER|TRADE_RECAP|STOP_ORDER|3_QUOTE|REG_SHO|SEC_LENDING|OTHER)
 	.rptRunDate(LocalDate.of(2017,03,22))
 	.rptInputStartDate()
 	.rptInputEndDate()
-	.rptFrequency(DAILY|INTRADAY|WEEKLY|MONTHLY|QUARTERLY|YEALRY|CUSTOM)
+	.rptFrequency(DAILY|INTRADAY|WEEKLY|MONTHLY|QUARTERLY|YEALRY|CRON_EXPRESSION)
 	# .rptIntradayRunTime(LocalTime.of(09,30), LocalTime.of(16,00)) -- only required for intraday reports
 	.rptOutputDir()	
 	.rptOutputFileNames(r_{report_type}_{report_sub_type}_{input_date(s)}.csv)  -- if multiple files, delivered in order specified
@@ -50,6 +51,8 @@ ReportConfig
  		
  		--more email configs, if same report needs to be sent with different subject/body or for privacy
  		)
+	.emailEnabled(true)
+	.emailRedirectToDevEnabled(false) -- Used by testMode to redirect email to dev
  	.ftp (
  		new FtpConfig()
  		 .ftpHost()
@@ -58,12 +61,14 @@ ReportConfig
  		 .ftpPasswd()
  		 .ftpDir()
  		 .ftpFileName(defaults to reportFileName)
- 		 .retryAfter(15, TimeUnit.MINUTES)
- 		 .retryAttempts(-1) -1 = never
+ 		 .ftpRetryAfter(15, TimeUnit.MINUTES)
+ 		 .ftpRetryAttempts(-1) -1 = never
+		 .whenFtpFailed(devEmailConsumer,logConsumer,jsonConsumer)
  		 .build(),
  		 
  		 -- more ftp configs
  		)
+	.ftpEnabled(true)	-- Used by testMode to disable ftp
  	.sftp (
  		new SftpConfig()
  		 .sftpHost()
@@ -73,30 +78,35 @@ ReportConfig
  		 .sftpPort()
  		 .sftpDir()
  		 .sftpFileName(defaults to reportFileName)
+		 .sftpRetryAfter(15, TimeUnit.MINUTES)
+ 		 .sftpRetryAttempts(-1) -1 = never
+		 .whenSftpFailed(devEmailConsumer,logConsumer,jsonConsumer)
  		 .build()
  		 
  		 -- more sftp configs
  		)
-	.compress()
+	.sftpEnabled(true)   -- Used by testMode to disable sftp
+	.compressionEnabled(true)
 	.rptLogDir()
 	.rptLogFileName()
 	.rptLogDaysToKeep()
+	.testModeEnabled(true) -- Test Mode: FTP/SFTP disabled ; email to clients disabled;
+	.testDirectory() -- Must be specified if running in test mode
 	.build(); --calls validate, use validate directly to test config
 
 ReportScheduler
 	.rptId()
-	.dependsOn(job1, job2)  -- takes a list of predicates and tests them each time e.g. new StgLoadStatusPredicate(101)
-	.runOnWeekends(true)
-	.runOnUSHolidays(false)
-	.runOnUKHolidays(true)
 	.whenStarted(devEmailConsumer,jsonConsumer)
+	.dependsOn(job1, job2)  -- takes a list of predicates and tests them each time e.g. new StgLoadStatusPredicate(101)
 	.whenDependencyNotMet(devEmailConsumer,jsonConsumer)
 	.retryDependencyCheckAfter(15, TimeUnit.MINUTES)
 	.maxWaitTimeForDependency(LocalDateTime.of(rptRunDate, LocalTime.of(23,0)))  -- 11:00 PM of report run date  
 	.whenFailed(devEmailConsumer,jsonConsumer)
 	.whenDelayed(devEmailConsumer,jsonConsumer)
 	.whenCompleted(devEmailConsumer,jsonConsumer)
-	.frequency(DLY|MTH|EOM|QTR|Intraday|CronParser)
+	.runOnWeekends(true)
+	.runOnUSHolidays(false)
+	.runOnUKHolidays(true)
 	.build()
 	.schedule();
 	
