1bb83b148b73fa206c06b1393de6da02786a033b
ajitkumar10 <ajitkumar10@gmail.com>
2017-04-01 20:42:01 -0400

    Update ReportProcessor.java

--- ReportProcessor.java
+++ ReportProcessor.java
@@ -9,34 +9,83 @@ Monitoring and Alerts
 ReportConfig
 	.id()
 	.rptName("report {report_type} - {report_sub_type}")
+	.rptRunDate(LocalDate.of(2017,03,22))
+	.rptInputStartDate()
+	.rptInputEndDate()
+	.rptFrequency(DAILY|INTRADAY|WEEKLY|MONTHLY|QUARTERLY|YEALRY|CUSTOM)
+	# .rptIntradayRunTime(LocalTime.of(09,30), LocalTime.of(16,00)) -- only required for intraday reports
 	.rptOutputDir()	
-	.rptOutputFileName(r_openorder_ssic_$date.csv)
+	.rptOutputFileNames(r_{report_type}_{report_sub_type}_{input_date(s)}.csv)  -- if multiple files, delivered in order specified
+	.rptDoneFileRequired(true)
+	.rptDoneFileName('{report_client}.done')
 	.rptOutputFileDaysToKeep(5)
 	.rptInputDataSource(
-		new SqlSource(),
+		new OracleSource(),
 		-- or new TextFileSource()
-		-- or new DBSource()
+		-- or new SybaseSource()
 		)
 	.headerDefaultOverride() -- override header coming from datasource e.g. default header will be column names for SQL source
 	.headerOn()
 	.footerDefaultOverride() -- default is TLR|{record_Count}
 	.footerOn
 	.repeatHeaderFooterAfterLines()
-	.output(
+	.rptDataTransformer(
+		-- pipeline of transformers
+		new DelimitedTextTransformer("|").andThen(new PythonCustomClientFormatter())
 		new OutputConfig()
 		.format(delimited(txt)|csv|xls|xlsx|pdf)
 		.delimiter(',')
 		.build()
 	)
+	.email(
+ 		new EmailConfig()
+ 		.fromAddr()
+ 		.toAddr()
+ 		.ccAddr()
+ 		.bccAddr()
+ 		.subject()  -- Append Env 
+ 		.body()
+ 		.attachments(defaults to reportFileName)
+ 		.build()
+ 		
+ 		--more email configs, if same report needs to be sent with different subject/body or for privacy
+ 		)
+ 	.ftp (
+ 		new FtpConfig()
+ 		 .ftpHost()
+ 		 .ftpUser()
+ 		 .ftpPort()
+ 		 .ftpPasswd()
+ 		 .ftpDir()
+ 		 .ftpFileName(defaults to reportFileName)
+ 		 .retryAfter(15, TimeUnit.MINUTES)
+ 		 .retryAttempts(-1) -1 = never
+ 		 .build(),
+ 		 
+ 		 -- more ftp configs
+ 		)
+ 	.sftp (
+ 		new SftpConfig()
+ 		 .sftpHost()
+ 		 .sftpUser()
+ 		 .sftpPassword()
+		 .sftpIdentityFilePath()  -- either password is required or identity file path 
+ 		 .sftpPort()
+ 		 .sftpDir()
+ 		 .sftpFileName(defaults to reportFileName)
+ 		 .build()
+ 		 
+ 		 -- more sftp configs
+ 		)
 	.compress()
 	.rptLogDir()
 	.rptLogFileName()
 	.rptLogDaysToKeep()
-	.build(); --calls validate, use validate to test config
+	.build(); --calls validate, use validate directly to test config
 
-JobScheduler
-	.jobId()
-	.dependsOn(job1, job2)
+ReportScheduler
+	.rptId()
+	.dependsOn(job1, job2)  -- takes a list of predicates and tests them each time e.g. new StgLoadStatusPredicate(101)
 	.runOnWeekends(true)
 	.runOnUSHolidays(false)
 	.runOnUKHolidays(true)
