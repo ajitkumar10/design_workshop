086d92948acb65b4c04405a4eef05deb62a66b19
ajitkumar10 <ajitkumar10@gmail.com>
2017-04-14 21:33:50 -0400

    Update ReportProcessor.java

--- ReportProcessor.java
+++ ReportProcessor.java
@@ -9,14 +9,14 @@ Monitoring and Alerts
 ReportConfig
 	.id()
 	.rptName("report {report_type} - {report_sub_type}")
-	.rptType(OPEN_ORDER|TRADE_RECAP|STOP_ORDER|3_QUOTE|REG_SHO|SEC_LENDING|OTHER)
+	.rptType("{report_type}")
 	.rptRunDate(LocalDate.of(2017,03,22))
 	.rptInputStartDate()
 	.rptInputEndDate()
 	.rptFrequency(DAILY|INTRADAY|WEEKLY|MONTHLY|QUARTERLY|YEALRY|CRON_EXPRESSION)
 	# .rptIntradayRunTime(LocalTime.of(09,30), LocalTime.of(16,00)) -- only required for intraday reports
 	.rptOutputDir()	
-	.rptOutputFileNames(r_{report_type}_{report_sub_type}_{input_date(s)}.csv)  -- if multiple files, delivered in order specified
+	.rptOutputFileNames("{report_type}_{report_sub_type}_{input_date(s)}.csv")  -- if multiple files, delivered in order specified
 	.rptDoneFileRequired(true)
 	.rptDoneFileName('{report_client}.done')
 	.rptOutputFileDaysToKeep(5)
@@ -97,7 +97,7 @@ ReportConfig
 ReportScheduler
 	.rptId()
 	.whenStarted(devEmailConsumer,jsonConsumer)
-	.dependsOn(job1, job2)  -- takes a list of predicates and tests them each time e.g. new StgLoadStatusPredicate(101)
+	.dependsOn(job1, job2)  -- takes a list of predicates and tests them each time 
 	.whenDependencyNotMet(devEmailConsumer,jsonConsumer)
 	.retryDependencyCheckAfter(15, TimeUnit.MINUTES)
 	.maxWaitTimeForDependency(LocalDateTime.of(rptRunDate, LocalTime.of(23,0)))  -- 11:00 PM of report run date  
